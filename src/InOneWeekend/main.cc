4e78670d5f170a11558f32371eb35fa81ebe204f
NoahAHodge <noahahodge@gmail.com>
2026-08-18 15:15:22 -0500

    blue to white gradient with rays

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -4,9 +4,12 @@
 
 #include <iostream>>
 
-// return black for now
+// blue to white gradient depending on a ray's y coordinate
 color ray_color(const ray& r) {
-    return color(0,0,0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    // auto a = 0.5 * (unit_direction.x() + 1.0); // depending on ray's x coordinate
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main(){
