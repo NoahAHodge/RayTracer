0d4d1b7ddf6e5f80cd6cc4a1ce1c155a30809dd2
NoahAHodge <noahahodge@gmail.com>
2026-08-19 15:26:30 -0500

    gamma correction

--- src/InOneWeekend/color.h
+++ src/InOneWeekend/color.h
@@ -7,10 +7,22 @@
 
 using color = vec3;
 
+// transform linear space color components to gamma 2
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) 
+        return std::sqrt(linear_component);
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // transform to gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // translate 0-1 to 0-255
     static const interval intensity(0.000, 0.999);
