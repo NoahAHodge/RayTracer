3c43ad725c867c0271e324a6bb5c47361fb88bf9
NoahAHodge <noahahodge@gmail.com>
2026-08-19 14:08:57 -0500

    random in const

--- src/InOneWeekend/const.h
+++ src/InOneWeekend/const.h
@@ -2,6 +2,7 @@
 #define CONST_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -17,6 +18,16 @@ const double pi = 3.1415926535897932385;
 // utility fxns
 inline double degress_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // return random double in [0,1)
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // return random double in [min,max)
+    return min + (max-min) * random_double();
 }
 
 // headers
